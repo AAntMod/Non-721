37f9dd94e866de8cbaf573672820b7603cf0926b
Arnon Hongklay <arnon@hongklay.com>
2022-04-16 13:36:02 +0700

    Add nft file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("@ericxstone/hardhat-blockscout-verify");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,6 +25,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "eco",
   solidity: "0.8.4",
   networks: {
     hardhat: {
@@ -31,6 +35,12 @@ module.exports = {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    eco: {
+      url: "https://rpc-ecochain.antify.org",
+      chainId: 11,
+      gasPrice: 20000000000,
+      accounts: { mnemonic: process.env.MNEMONIC },
     },
   },
   gasReporter: {
